ac6b7bcd0eebd27e099231f36d5dfdde04619c23
OoO <xufooo@gmail.com>
2013-07-20 20:09:38 +0800

    add missing myhoverpoints

---
+++ myhoverpoints.cpp
@@ -0,0 +1,62 @@
+/*=============================================================================
+# Copyright (C) 2013 OoO
+#
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Description: 
+#
+#
+# Last modified: 2013-07-20 12:47
+#
+# Should you need to contact me, you can do so by 
+# email - mail your message to <xufooo@gmail.com>.
+=============================================================================*/
+
+#include "myhoverpoints.h"
+#include <QDebug>
+
+MyHoverPoints::MyHoverPoints(QWidget *parent, PointShape shape):HoverPoints(parent,shape)
+{
+	m_widget=parent;
+	setConnectionType(HoverPoints::HVLConnection);
+	qDebug()<<"parent.rect"<<parent->rect();
+}
+
+
+bool MyHoverPoints::eventFilter(QObject *object, QEvent *event)
+{
+	
+	if(object==m_widget){
+		switch(event->type()){
+			case QEvent::MouseButtonPress:
+			break;	
+//			case QEvent::Paint:
+//			object->event(event);
+		}
+		return true;
+	}
+	
+//	HoverPoints::eventFilter(object,event);
+}
+
+void MyHoverPoints::paintPoints()
+{
+//	if(!isVisible()){
+		qDebug()<<"not visible";
+		return;
+//	}
+//	HoverPoints::paintPoints();
+}
+
---
+++ myhoverpoints.h
@@ -0,0 +1,47 @@
+/*=============================================================================
+# Copyright (C) 2013 OoO
+#
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Description: 
+# Use this module to add hoverpoints to qobjects for scaling them. inherits HoverPoints.
+#
+# Last modified: 2013-07-20 12:19
+#
+# Should you need to contact me, you can do so by 
+# email - mail your message to <xufooo@gmail.com>.
+=============================================================================*/
+
+#ifndef MYHOVERPOINTS_H
+#define MYHOVERPOINTS_H
+
+#include "hoverpoints.h"
+
+class MyHoverPoints:public HoverPoints
+{
+	Q_OBJECT;
+public:
+	MyHoverPoints(QWidget *parent, PointShape shape=HoverPoints::CircleShape);
+
+	bool eventFilter(QObject *object, QEvent *event);
+
+	void paintPoints();
+	
+private:
+	QWidget *m_widget;
+};
+
+
+#endif
